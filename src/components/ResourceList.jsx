a274533dbab42b798e36c23f8607eca00127893f
Jeong Min Lee <jacklee0223@gmail.com>
2020-02-18 00:55:39 -0600

    implement resourcelist

--- src/components/ResourceList.jsx
+++ src/components/ResourceList.jsx
@@ -1,1 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
+  const fetchResource = async resource => {
+    const response = await Axios.get(
+      `https://jsonplaceholder.typicode.com/${resource}`
+    );
+
+    setResources(response.data);
+  };
+
+  useEffect(() => {
+    fetchResource(resource);
+  }, [resource]);
+
+  return <div>{resources.length}</div>;
+};
+
+export default ResourceList;
