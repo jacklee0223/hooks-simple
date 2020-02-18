9640b45fafd6c1bfe03a0d42715b00e1f9fc9687
Jeong Min Lee <jacklee0223@gmail.com>
2020-02-18 14:23:06 -0600

    refactor useEffect into reusable function

--- src/components/ResourceList.jsx
+++ src/components/ResourceList.jsx
@@ -1,26 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
   const [resources, setResources] = useState([]);
-  const fetchResource = async resource => {
-    const response = await Axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-
-    setResources(response.data);
-  };
 
   useEffect(() => {
-    fetchResource(resource);
-    // (async resource => {
-    //   const response = await Axios.get(
-    //     `https://jsonplaceholder.typicode.com/${resource}`
-    //   );
-
-    //   setResources(response.data);
-    // })(resource);
+    // fetchResource(resource);
+    (async resource => {
+      const response = await Axios.get(
+        `https://jsonplaceholder.typicode.com/${resource}`
+      );
+      setResources(response.data);
+    })(resource);
   }, [resource]);
+
+  return resources;
+};
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
+  // const [resources, setResources] = useState([]);
+  // // const fetchResource = async resource => {
+  // //   const response = await Axios.get(
+  // //     `https://jsonplaceholder.typicode.com/${resource}`
+  // //   );
+
+  // //   setResources(response.data);
+  // // };
+
+  // useEffect(() => {
+  //   // fetchResource(resource);
+  //   (async resource => {
+  //     const response = await Axios.get(
+  //       `https://jsonplaceholder.typicode.com/${resource}`
+  //     );
+  //     setResources(response.data);
+  //   })(resource);
+  // }, [resource]);
 
   return (
     <ul>
